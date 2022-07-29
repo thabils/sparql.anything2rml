07f47470f02431c82ab4d56bf36d72d50af05dc9
Tibe Habils <tibe.habils@ugent.be>
2022-07-29 12:26:28 +0200

    - testing now also works from console (see README) - added jar pathway to config file

--- src/util.py
+++ src/util.py
@@ -15,8 +15,10 @@ def find_literal(list_nodes):
 
 def call_sparql_anything_jar(directory, output_file):
     config = configparser.ConfigParser()
-    config.read("../config.ini")
+    config.read("config.ini")
+    print(config.sections())
     jar_version = config.get("JAR", "VERSION")
+    print(jar_version)
 
     subprocess.call(
         ['java', '-jar', jar_version, '-q', directory + '/query.sparql', '-f', 'NQ', '-o', output_file])
