974f5248560cb06b0497dd3ee10eb6d96484de1b
Tibe Habils <tibe.habils@ugent.be>
2022-08-01 11:42:58 +0200

    add test mode to cli

--- src/cli.py
+++ src/cli.py
@@ -1,19 +1,28 @@
 import sys
 
 from main import generate_sparql_anything
-
+from util import call_sparql_anything_jar, compare_n3_files
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         print('You have specified too many arguments')
         sys.exit()
 
-    if len(sys.argv) < 2:
-        print('You need to specify the mapping file')
+    if len(sys.argv) < 3:
+        print('You need to specify the mapping file and mode')
         sys.exit()
 
     input_file = sys.argv[1]
-
+    mode = sys.argv[2]
     print(input_file)
-    generate_sparql_anything(input_file)
+    if mode == "generate":
+        generate_sparql_anything(input_file)
+    if mode == "test":
+        generate_sparql_anything(input_file)
+        test_case_directory = input_file[:input_file.rfind("/")]
+        call_sparql_anything_jar(test_case_directory, test_case_directory + "/" + "sparql_output.nq")
+        if compare_n3_files(test_case_directory + "/sparql_output.nq", test_case_directory + "/output.nq"):
+            print("Correct!")
+        else:
+            print("False")
 
