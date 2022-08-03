3712ed8fd73ecf90dead3e6982afe031dcc658df
Tibe Habils <tibe.habils@ugent.be>
2022-08-03 13:43:52 +0200

    Add printing statments to cli and run mode

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,7 @@
 import sys
 
 from main import generate_sparql_anything
-from util import call_sparql_anything_jar, compare_n3_files
+from util import call_sparql_anything_jar
 
 if __name__ == "__main__":
     if len(sys.argv) > 3:
@@ -16,9 +16,15 @@ if __name__ == "__main__":
     mode = sys.argv[2]
     print(input_file)
     if mode == "create":
+        print("creating file")
         generate_sparql_anything(input_file)
     elif mode == "generate":
+        print("generating file")
         generate_sparql_anything(input_file)
+        test_case_directory = input_file[:input_file.rfind("/")]
+        call_sparql_anything_jar(test_case_directory, test_case_directory + "/" + "sparql_output.nq")
+    elif mode == "run":
+        print("running file")
         test_case_directory = input_file[:input_file.rfind("/")]
         call_sparql_anything_jar(test_case_directory, test_case_directory + "/" + "sparql_output.nq")
 
